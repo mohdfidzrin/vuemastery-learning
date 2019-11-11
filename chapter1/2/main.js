aece20ece76672d496ed76ffc49d0fb58ff97974
Mohd Fidzrin Bin Mohd Hamizaz <mohdfidzrin@plenitude-solutions.com>
2019-11-11 20:39:40 +0800

    new lesson 2. Attribute Binding

---
+++ chapter1/2/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "socks",
+    description: "This is a limited edition socks released on 11-11-2019",
+    origin: "Malaysia",
+    image: "./assets/socks-green.jpg",
+    link: "https://www.lazada.com.my/catalog/?q=green+socks"
+  }
+});
